1dae29a73213194603398eeae15659bda6aa740a
Domyygamer <91354992+Domyygamer@users.noreply.github.com>
2022-03-05 22:44:15 +0200

    ''

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import { convert12HoursSystem } from "/modules.js";
-
 window.addEventListener("load", () => toggleLoadingSceen());
 
 //* nav btn actions
@@ -190,6 +188,19 @@ function createDaysForecast(dayForecast, dayNumber) {
   }
 
   holder.append(template);
+}
+
+function convert12HoursSystem(date) {
+  // getting the time part from the date by splitting on space
+  // then splitting the time to convert the first part
+  let time = date.toString().split(" ")[1].split(":");
+  if (time[0] > 12) {
+    time[0] -= 12;
+    time[1].padStart(2, "0");
+    return `${time.join(":")} pm`;
+  } else {
+    return `${time.join(":")} am`;
+  }
 }
 
 //! user changes the units
